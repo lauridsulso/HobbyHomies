9830ec7329cf48834624fd09e53a9322ebc7c734
majahoberg <eaamhob@students.eaaa.dk>
2020-09-15 11:48:43 +0200

    hidetabbar

--- js/main.js
+++ js/main.js
@@ -34,3 +34,27 @@ function appendCategories(categories) {
   }
 
 // hobby api link: https://gist.githubusercontent.com/carlelieser/884584d06b2d9429f321ec192f6dc7b5/raw/0888b5449ecda4787001b74811e645d0a74b8132/hobbies.json
+
+function hashTabbar(id) {
+  let array = [
+    "loader", "login", "signin"
+  ];
+
+  for(const navn of array) {
+    if (navn === id) {
+      return true
+    }
+  }
+
+  return false
+}
+
+// show and hide tabbar
+function hideTabbar(hide) {
+  let tabbar = document.querySelector('#tabbar');
+  if (hide) {
+    tabbar.classList.add("hide");
+  } else {
+    tabbar.classList.remove("hide");
+  }
+}
